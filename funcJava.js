e811fcbcd1201949118127751a42c0629ece48f9
close01 <sirinanp64@gmail.com>
2021-01-15 00:08:11 +0700

    text

--- funcJava.js
+++ funcJava.js
@@ -4,25 +4,25 @@ function loopItem(){
     for (i = 0, len = items.length, text = ""; i < len; i++) {
         text += 
         `<div class='box-out' id='${i}'>
-                <div class='out' id='out'>${items[i]}</div>
-                <button class='bg-green' onClick='editItem(${i})'>edit</button>
-                <button class='bg-red' onClick='deleteItem(${i})'>delete</button>
+                <p class='output' id='out'>${items[i]}</p>
+                <button class='edit-btn' onClick='editItem(${i})'>Edit</button>
+                <button class='delete-btn' onClick='deleteItem(${i})'>Delete</button>
             </div>`;
     }
     return text;
 }
 function updateItem(newItem){
         textNewItem = 
-            `<div class='out' id='out'>${items[newItem]}</div>
-            <button class='bg-green' onClick='editItem(${newItem})'>edit</button>
-            <button class='bg-red' onClick='deleteItem(${newItem})'>delete</button>`;
+            `<p class='output' id='out'>${items[newItem]}</p>
+            <button class='edit-btn' onClick='editItem(${newItem})'>Edit</button>
+            <button class='delete-btn' onClick='deleteItem(${newItem})'>Delete</button>`;
     return textNewItem;
 }
 function addItems() { 
 
     if(document.getElementById('inputText').value == "")
     {
-        alert('PLEASE INPUT DATA');
+        alert('PLEASE INPUT');
         return false;
     }
     else{
@@ -33,14 +33,13 @@ function addItems() {
         document.getElementById("show-output").innerHTML = loopItem();
         document.getElementById("inputText").value ="";
     }
-    
 }
 
 function editItem(edit_id){
     var inputEdit =
     `<div id = 'e${edit_id}'>
         <input id='inputNewEdit${edit_id}' type='text' value='${items[edit_id]}'/>
-        <button class='bg-blue' id='editbtn' onClick='editItemDone(${edit_id})'>done</button>
+        <button class='bg-blue' id='editbtn' onClick='editItemDone(${edit_id})'>Done</button>
     </div>`;
     document.getElementById(edit_id).innerHTML = inputEdit;
 }
@@ -48,24 +47,20 @@ function editItemDone(edit_done){
 
     if(document.getElementById("inputNewEdit"+edit_done.toString()).value == "")
     {
-        alert('PLEASE INPUT DATA');
+        alert('PLEASE INPUT');
         return false;
     }
     else{
         var y = document.getElementById("inputNewEdit"+edit_done.toString()).value;
         items.splice(edit_done, 1, y);
-        // document.getElementById("output").innerHTML = items; 
         console.log(items);
         document.getElementById(edit_done.toString()).innerHTML = updateItem(edit_done);
-    }
-    
+    } 
 }
-
 
 function deleteItem(delete_id) {
     items.splice(delete_id, 1);
     console.log(items);
-    // document.getElementById("output").innerHTML = items;
     document.getElementById("show-output").innerHTML = loopItem();
 }
 var input = document.getElementById("inputText");
@@ -76,16 +71,4 @@ input.addEventListener("keydown", function(event) {
         document.getElementById("addbtn").click();
     }
     console.log(event, 'event');
-// event.preventDefault();
     });
-// var inputNew = document.getElementById("inputNewEdit");
-// inputNew.addEventListener("keyup", (event) => {
-//         console.log("asdf");
-//         if (event.keyCode === 13) {
-//             console.log(event.keyCode, 'keyCode');
-//             event.preventDefault();
-//             document.getElementById("editbtn").click();
-//         }
-//         console.log(event, 'event');
-//         // event.preventDefault();
-//     });
